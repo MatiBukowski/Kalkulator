7855bd9bf400df71fe162e1b4d24731924c95c9f
MatiBukowski <mati.bukowski@icloud.com>
2023-05-22 21:23:34 +0200

    converter commit 1

--- src/main/java/com/example/kalkulator/ConvertController.java
+++ src/main/java/com/example/kalkulator/ConvertController.java
@@ -1,6 +1,10 @@
 package com.example.kalkulator;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class ConvertController {
     private MainController mainController;
@@ -13,4 +17,60 @@ public class ConvertController {
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
+
+    @FXML
+    private ChoiceBox length1id;
+    @FXML
+    private ChoiceBox length2id;
+    @FXML
+    private Label lengthResult;
+    @FXML
+    private TextField text;
+
+    ObservableList<String> lengthChoice = FXCollections.observableArrayList("metry", "mile", "cale");
+
+    public void initialize() {
+        lengthOperation();
+    }
+
+    public void lengthOperation() {
+        length1id.setValue("metry");
+        length2id.setValue("mile");
+
+        length1id.setItems(lengthChoice);
+        length2id.setItems(lengthChoice);
+    }
+
+    @FXML
+    public void convert() {
+        double value = Double.valueOf(text.getText());
+
+        if(length1id.getValue().equals("metry") && length2id.getValue().equals("mile")) {
+            value = value*0.000621371192;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else if(length1id.getValue().equals("metry") && length2id.getValue().equals("cale")) {
+            value = value*39.3700787;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else if(length1id.getValue().equals("mile") && length2id.getValue().equals("metry")) {
+            value = value*1609.344;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else if(length1id.getValue().equals("mile") && length2id.getValue().equals("cale")) {
+            value = value*63360 ;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else if(length1id.getValue().equals("cale") && length2id.getValue().equals("metry")) {
+            value = value*0.0254 ;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else if(length1id.getValue().equals("cale") && length2id.getValue().equals("mile")) {
+            value = value*0.000015782828 ;
+            lengthResult.setText(String.valueOf(value));
+        }
+        else
+            lengthResult.setText(String.valueOf(value));
+    }
+
 }
